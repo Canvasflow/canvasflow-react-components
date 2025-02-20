400a56f307c11d6b1c96060a8716cff6cb77ffcc
Jose Juan <josejuan2412@hotmail.com>
2025-02-20 16:04:45 -0500

    feat: image component

---
+++ lib/components/article/components/Image.tsx
@@ -0,0 +1,286 @@
+import { OnOff, Devices } from "./Types";
+
+export const Image = (props: ImageProps) => {
+  const {
+    id,
+    fullwidth,
+    align,
+    expandfullwidth,
+    bleed,
+    animation,
+    imagelink,
+    captionenabled,
+    fixedwidth,
+    creditenabled,
+    imageurl,
+    htmlclass,
+    imageclip,
+    lightbox,
+    externallinktarget,
+    linktype,
+    onlyShowCaptionInLightbox,
+    onlyShowCreditInLightbox,
+    style,
+    caption,
+    credit,
+    width,
+    component,
+    lang,
+  } = props;
+
+  let classNames = ["media", component];
+  const containerStyle: any = {};
+  const imageStyle: any = {
+    pointerEvents: "none",
+  };
+  const captionStyle: any = {};
+
+  if (htmlclass && htmlclass.length) {
+    classNames = [...classNames, ...htmlclass];
+  }
+
+  switch (align) {
+    case "left":
+      containerStyle.textAlign = "left";
+      // imageStyle['margin-right'] = 'auto';
+      // containerStyle.alignItems = 'flex-start';
+      break;
+    case "right":
+      containerStyle.textAlign = "right";
+      // imageStyle['margin-left'] = 'auto';
+      // containerStyle.alignItems = 'flex-end';
+      break;
+    case "float-left":
+      //classNames.push(imageStyles['float-left'], 'float');
+      break;
+    case "float-right":
+      //classNames.push(imageStyles['float-right'], 'float');
+      break;
+    default:
+      containerStyle.textAlign = "center";
+      /*imageStyle['margin-right'] = 'auto';
+        imageStyle['margin-left'] = 'auto';*/
+      break;
+  }
+
+  if (fixedwidth && width && !fullwidth) {
+    imageStyle.width = width;
+    captionStyle.width = width;
+  }
+
+  if (fullwidth) {
+    imageStyle.maxWidth = "100%";
+  }
+
+  imageStyle.clipPath = getClipPath(imageclip);
+
+  switch (bleed) {
+    case "on":
+      classNames.push("bleed");
+      break;
+    case "left":
+    case "right":
+      classNames.push(`bleed-${bleed}`);
+      break;
+    default:
+      break;
+  }
+
+  if (style) {
+    setImageInlineStyles(containerStyle, style);
+  }
+
+  const imageComponent = (
+    <img
+      src={`${imageurl.replace(/\\/g, "/")}`}
+      alt={caption || ""}
+      height={1000}
+      style={imageStyle}
+    />
+  );
+
+  return (
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+    <figure
+      id={id}
+      className={classNames.join(" ")}
+      style={containerStyle}
+      //   onKeyDown={click}
+      //   onClick={click}
+    >
+      {imageComponent}
+      {/* {captionenabled && caption && !onlyShowCaptionInLightbox ? (
+        <VirtualCaption style={captionStyle} content={caption} />
+      ) : null}
+      {creditEnabled && credit && !onlyShowCreditInLightbox ? (
+        <Credit style={captionStyle} content={credit} />
+      ) : null} */}
+    </figure>
+  );
+};
+
+interface ImageProps {
+  id: string;
+  align?: string;
+  bleed?: OnOff | "left" | "right";
+  style: any;
+  fullwidth?: OnOff;
+  expandfullwidth?: OnOff;
+  externallinktarget?: string;
+  caption?: string;
+  captionenabled?: OnOff;
+  captionposition?: string;
+  credit?: string;
+  animation?: any;
+  creditenabled?: OnOff;
+  width?: number | null;
+  fixedwidth?: OnOff;
+  link?: string;
+  url: string;
+  imageclip: string;
+  linktype: string;
+  lightbox: OnOff;
+  htmlclass: Array<any>;
+  onlyShowCaptionInLightbox: OnOff;
+  onlyShowCreditInLightbox: OnOff;
+  component: string;
+  lang?: string;
+  imageurl: string;
+  imagelink: string;
+}
+
+function getClipPath(imageClip: string) {
+  switch (imageClip) {
+    case "circle":
+      return "circle(50% at 50% 50%)";
+    case "ellipse":
+      return "ellipse(25% 40% at 50% 50%)";
+    case "triangle":
+      return "polygon(50% 0%, 0% 100%, 100% 100%)";
+    case "trapezoid":
+      return "polygon(20% 0%, 80% 0%, 100% 100%, 0% 100%)";
+    case "parallelogram":
+      return "polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)";
+    case "rhombus":
+      return "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)";
+    case "pentagon":
+      return "polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%)";
+    case "hexagon":
+      return "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)";
+    case "heptagon":
+      return "polygon(50% 0%,90% 20%, 100% 60%,75% 100%, 25% 100%,0% 60%,10% 20%)";
+    case "octagon":
+      return "polygon(30% 0%, 70% 0%, 100% 30%, 100% 70%, 70% 100%, 30% 100%, 0% 70%, 0% 30%)";
+    case "nonagon":
+      return "polygon(50% 0%, 83% 12%, 100% 43%, 94% 78%, 68% 100%, 32% 100%, 6% 78%, 0% 43%, 17% 12%)";
+    case "decogon":
+      return "polygon(50% 0%, 80% 10%, 100% 35%, 100% 70%, 80% 90%, 50% 100%, 20% 90%, 0% 70%, 0% 35%, 20% 10%)";
+    case "bevel":
+      return "polygon(20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%)";
+    case "rabbet":
+      return "polygon(0% 15%, 15% 15%, 15% 0%, 85% 0%, 85% 15%, 100% 15%, 100% 85%, 85% 85%, 85% 100%, 15% 100%, 15% 85%, 0% 85%)";
+    case "leftarrow":
+      return "polygon(40% 0%, 40% 20%, 100% 20%, 100% 80%, 40% 80%, 40% 100%, 0% 50%)";
+    case "rightarrow":
+      return "polygon(0% 20%, 60% 20%, 60% 0%, 100% 50%, 60% 100%, 60% 80%, 0% 80%)";
+    case "leftpoint":
+      return "polygon(25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%)";
+    case "rightpoint":
+      return "polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)";
+    case "rightchevron":
+      return "polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%)";
+    case "leftchevron":
+      return "polygon(100% 0%, 75% 50%, 100% 100%, 25% 100%, 0% 50%, 25% 0%)";
+    case "star":
+      return "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+    case "close":
+      return "polygon(20% 0%, 0% 20%, 30% 50%, 0% 80%, 20% 100%, 50% 70%, 80% 100%, 100% 80%, 70% 50%, 100% 20%, 80% 0%, 50% 30%)";
+    default:
+      return null;
+  }
+}
+
+function setImageInlineStyles(containerStyle: any, style: any) {
+  if (!style) {
+    return;
+  }
+
+  if (!style.unit) {
+    return;
+  }
+
+  const { unit } = style;
+
+  const attributes: any = {
+    margin_top: "marginTop",
+    margin_right: "marginRight",
+    margin_bottom: "marginBottom",
+    margin_left: "marginLeft",
+    padding_top: "paddingTop",
+    padding_right: "paddingRight",
+    padding_bottom: "paddingBottom",
+    padding_left: "paddingLeft",
+  };
+
+  for (const attr in attributes) {
+    if (style[attr] !== undefined && unit[attr]) {
+      containerStyle[attributes[attr]] = `${style[attr]}${unit[attr]}`;
+    }
+  }
+}
+
+// {
+//     "fullwidth": "off",
+//     "onlyShowCaptionInLightbox": false,
+//     "lightbox": "off",
+//     "imageframestyle": "none",
+//     "aspectratio": "free",
+//     "align": "center",
+//     "expandfullwidth": "off",
+//     "onlyShowCreditInLightbox": false,
+//     "animation": {
+//       "type": "none",
+//       "params": {
+//         "speed": "medium",
+//         "delay": "1",
+//         "repeat": "0"
+//       }
+//     },
+//     "bleed": "off",
+//     "caption": "",
+//     "role": "image",
+//     "width": 300,
+//     "linktype": "none",
+//     "credit": "",
+//     "alt": "",
+//     "rawimage": "off",
+//     "externallinktarget": "on",
+//     "unselectable": "off",
+//     "imagefilter": "none",
+//     "imageclip": "none",
+//     "hpadding": 0,
+//     "imagelink": "",
+//     "captionposition": "below",
+//     "imageurl": "https://admin.cflowdev.com/usercontent/143476/611E08D7-9BF1-446E-AF18C84964F205BB.jpg",
+//     "templateId": 143476,
+//     "captionenabled": "off",
+//     "cacheparam": 1,
+//     "devices": {
+//       "tablet": "on",
+//       "desktop": "on",
+//       "phone": "on"
+//     },
+//     "fixedwidth": "off",
+//     "pagelink": "none",
+//     "tag": "",
+//     "creditenabled": "off",
+//     "htmlclass": "",
+//     "id": "Cf2-1565384310",
+//     "articleid": 143476,
+//     "imgunselectable": "off",
+//     "title": "",
+//     "component": "image",
+//     "behaviour": {
+//       "type": "none"
+//     }
+//   }
--- lib/components/article/components/Text.tsx
+++ lib/components/article/components/Text.tsx
@@ -1,134 +1,134 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { OnOff, Devices } from './Types';
-import styles from './../article.module.css';
+import { OnOff, Devices } from "./Types";
+import styles from "./../article.module.css";
 export const Text = (props: TextProps) => {
-	const {
-		id,
-		bleed = 'off',
-		text,
-		expandfullwidth = 'off',
-		component,
-		dropcap = 'off',
-		style,
-		imageurl,
-		imageenabled,
-		IMAGECAPTION,
-		devices,
-		text_lang,
-		lang,
-	} = props;
+  const {
+    id,
+    bleed = "off",
+    text,
+    expandfullwidth = "off",
+    component,
+    dropcap = "off",
+    style,
+    imageurl,
+    imageenabled,
+    IMAGECAPTION,
+    devices,
+    text_lang,
+    lang,
+  } = props;
 
-	const classNames: Array<string> = ['component', 'text', component];
+  const classNames: Array<string> = ["component", "text", component];
 
-	// Set text content base on language
-	let content = text;
-	if (lang && text_lang && text_lang[lang]) {
-		content = text_lang[lang];
-	}
-	let image = '';
-	let clearer = '';
+  // Set text content base on language
+  let content = text;
+  if (lang && text_lang && text_lang[lang]) {
+    content = text_lang[lang];
+  }
+  let image = "";
+  let clearer = "";
 
-	const componentStyle: any = {};
+  const componentStyle: any = {};
 
-	if (expandfullwidth === 'on') {
-		componentStyle.minWidth = '100%';
-		componentStyle.width = '100%';
-	}
+  if (expandfullwidth === "on") {
+    componentStyle.minWidth = "100%";
+    componentStyle.width = "100%";
+  }
 
-	if (style && style['unit']) {
-		const unit = style['unit'];
-		if (style['margin_top'] && unit['margin_top']) {
-			componentStyle.marginTop = `${style['margin_top']}${unit['margin_top']}`;
-		}
-		if (style['margin_left'] && unit['margin_left']) {
-			componentStyle.marginLeft = `${style['margin_left']}${unit['margin_left']}`;
-		}
-		if (style['margin_right'] && unit['margin_right']) {
-			componentStyle.marginRight = `${style['margin_right']}${unit['margin_right']}`;
-		}
-		if (style['margin_bottom'] && unit['margin_bottom']) {
-			componentStyle.marginBottom = `${style['margin_bottom']}${unit['margin_bottom']}`;
-		}
-		if (style['padding_top'] && unit['padding_top']) {
-			componentStyle.paddingTop = `${style['padding_top']}${unit['padding_top']}`;
-		}
-		if (style['padding_left'] && unit['padding_left']) {
-			componentStyle.paddingLeft = `${style['padding_left']}${unit['padding_left']}`;
-		}
-		if (style['padding_right'] && unit['padding_right']) {
-			componentStyle.paddingRight = `${style['padding_right']}${unit['padding_right']}`;
-		}
-		if (style['padding_bottom'] && unit['padding_bottom']) {
-			componentStyle.paddingBottom = `${style['padding_bottom']}${unit['padding_bottom']}`;
-		}
-	}
+  if (style && style["unit"]) {
+    const unit = style["unit"];
+    if (style["margin_top"] && unit["margin_top"]) {
+      componentStyle.marginTop = `${style["margin_top"]}${unit["margin_top"]}`;
+    }
+    if (style["margin_left"] && unit["margin_left"]) {
+      componentStyle.marginLeft = `${style["margin_left"]}${unit["margin_left"]}`;
+    }
+    if (style["margin_right"] && unit["margin_right"]) {
+      componentStyle.marginRight = `${style["margin_right"]}${unit["margin_right"]}`;
+    }
+    if (style["margin_bottom"] && unit["margin_bottom"]) {
+      componentStyle.marginBottom = `${style["margin_bottom"]}${unit["margin_bottom"]}`;
+    }
+    if (style["padding_top"] && unit["padding_top"]) {
+      componentStyle.paddingTop = `${style["padding_top"]}${unit["padding_top"]}`;
+    }
+    if (style["padding_left"] && unit["padding_left"]) {
+      componentStyle.paddingLeft = `${style["padding_left"]}${unit["padding_left"]}`;
+    }
+    if (style["padding_right"] && unit["padding_right"]) {
+      componentStyle.paddingRight = `${style["padding_right"]}${unit["padding_right"]}`;
+    }
+    if (style["padding_bottom"] && unit["padding_bottom"]) {
+      componentStyle.paddingBottom = `${style["padding_bottom"]}${unit["padding_bottom"]}`;
+    }
+  }
 
-	// Works if we want to embbed image in text component
-	if (imageenabled) {
-		if (imageurl) {
-			const caption = IMAGECAPTION
-				? `<figcaption>${IMAGECAPTION}</figcaption>`
-				: '';
-			componentStyle.display = 'block';
-			image = `<figure><img src="${imageurl}" />${caption}</figure>`;
-			clearer = '<span style="display: block; clear: both;"></span>';
-		}
-	}
+  // Works if we want to embbed image in text component
+  if (imageenabled) {
+    if (imageurl) {
+      const caption = IMAGECAPTION
+        ? `<figcaption>${IMAGECAPTION}</figcaption>`
+        : "";
+      componentStyle.display = "block";
+      image = `<figure><img src="${imageurl}" />${caption}</figure>`;
+      clearer = '<span style="display: block; clear: both;"></span>';
+    }
+  }
 
-	if (bleed === 'on') {
-		classNames.push('bleed');
-	}
+  if (bleed === "on") {
+    classNames.push("bleed");
+  }
 
-	if (dropcap === 'on') {
-		classNames.push('dropcap');
-	}
+  if (dropcap === "on") {
+    classNames.push("dropcap");
+  }
 
-	// Hide component in device size
-	if (devices.phone === 'off') {
-		classNames.push(styles['hide-mobile']);
-	}
+  // Hide component in device size
+  if (devices.phone === "off") {
+    classNames.push(styles["hide-mobile"]);
+  }
 
-	if (devices.tablet === 'off') {
-		classNames.push(styles['hide-tablet']);
-	}
+  if (devices.tablet === "off") {
+    classNames.push(styles["hide-tablet"]);
+  }
 
-	if (devices.desktop === 'off') {
-		classNames.push(styles['hide-desktop']);
-	}
+  if (devices.desktop === "off") {
+    classNames.push(styles["hide-desktop"]);
+  }
 
-	return (
-		<div
-			id={id}
-			style={componentStyle}
-			className={classNames.map((c) => c?.trim()).join(' ')}
-			dangerouslySetInnerHTML={{
-				__html: `${image} ${content} ${clearer}`,
-			}}
-		/>
-	);
+  return (
+    <div
+      id={id}
+      style={componentStyle}
+      className={classNames.map((c) => c?.trim()).join(" ")}
+      dangerouslySetInnerHTML={{
+        __html: `${image} ${content} ${clearer}`,
+      }}
+    />
+  );
 };
 
 interface TextProps {
-	id?: string;
-	IMAGECAPTION?: string;
-	text_lang?: any;
-	unit?: any;
-	bleed?: OnOff;
-	animation: any;
-	devices: Devices;
-	text?: any | string;
-	expandfullwidth?: OnOff;
-	css?: string;
-	imageenabled?: OnOff;
-	imageurl?: string;
-	imagemargin?: string;
-	imagefloat?: 'left' | 'right';
-	imageWidth?: string;
-	style?: any;
-	dropcap?: OnOff;
-	component: string;
-	lang?: string;
+  id: string;
+  IMAGECAPTION?: string;
+  text_lang?: any;
+  unit?: any;
+  bleed?: OnOff;
+  animation: any;
+  devices: Devices;
+  text?: any | string;
+  expandfullwidth?: OnOff;
+  css?: string;
+  imageenabled?: OnOff;
+  imageurl?: string;
+  imagemargin?: string;
+  imagefloat?: "left" | "right";
+  imageWidth?: string;
+  style?: any;
+  dropcap?: OnOff;
+  component: string;
+  lang?: string;
 }
 
 /*
