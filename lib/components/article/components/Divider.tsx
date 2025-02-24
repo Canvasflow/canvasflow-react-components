5634b82a2e3bef180adc461ba600e9cbbd8654f5
Jose Juan <josejuan2412@hotmail.com>
2025-02-24 16:01:29 -0500

    feat: divider component implementation

--- lib/components/article/components/Divider.tsx
+++ lib/components/article/components/Divider.tsx
@@ -1,11 +1,30 @@
 import { ReactElement } from "react";
 import { Canvasflow } from "../../../Canvasflow";
-// TODO Implement divider
+
 export const Divider = (
   props: Canvasflow.Component.Divider,
 ): ReactElement | null => {
-  console.log(props);
-  return null;
+  const { id, bleed, style } = props;
+  const classNames = ["divider"];
+
+  const containerStyle: any = {
+    clear: "both",
+  };
+
+  if (style) {
+    classNames.push(style);
+  }
+  if (bleed === "on") {
+    classNames.push("bleed");
+  }
+
+  const componentStyle: any = {};
+
+  return (
+    <div id={id} style={containerStyle} className={classNames.join(" ")}>
+      <hr style={componentStyle} />
+    </div>
+  );
 };
 
 export default Divider;
