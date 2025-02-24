8af63696f5e4d6462e77fefc83f5395a95dda18b
Jose Juan <josejuan2412@hotmail.com>
2025-02-24 16:02:49 -0500

    feat: spacer component implementation

--- lib/components/article/components/Spacer.tsx
+++ lib/components/article/components/Spacer.tsx
@@ -1,11 +1,43 @@
 import { ReactElement } from "react";
 import { Canvasflow } from "../../../Canvasflow";
-// TODO Implement spacer
+
 export const Spacer = (
   props: Canvasflow.Component.Spacer,
 ): ReactElement | null => {
-  console.log(props);
-  return null;
+  const { id, bleed, margin } = props;
+  const containerStyle: any = {
+    clear: "both",
+  };
+
+  const className = ["spacer"];
+
+  switch (margin) {
+    case "margin-1":
+      className.push("v-small");
+      break;
+    case "margin-20":
+      className.push("small");
+      break;
+    case "margin-75":
+      className.push("large");
+      break;
+    case "margin-100":
+      className.push("v-large");
+      break;
+    default:
+      className.push("medium");
+      break;
+  }
+
+  if (bleed) {
+    className.push("bleed");
+  }
+
+  return (
+    <div id={id} className={className.join(" ")} style={containerStyle}>
+      <br />
+    </div>
+  );
 };
 
 export default Spacer;
